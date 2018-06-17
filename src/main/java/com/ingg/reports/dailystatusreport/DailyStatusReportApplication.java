0a6b76cbd5f3b9d3e6d1a8adfa904291fbc234da
Belley Bala <balabelley@gmail.com>
2018-06-17 11:13:41 +0100

    Initial commit

---
+++ src/main/java/com/ingg/reports/dailystatusreport/DailyStatusReportApplication.java
@@ -0,0 +1,12 @@
+package com.ingg.reports.dailystatusreport;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DailyStatusReportApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DailyStatusReportApplication.class, args);
+	}
+}
---
+++ src/test/java/com/ingg/reports/dailystatusreport/DailyStatusReportApplicationTests.java
@@ -0,0 +1,16 @@
+package com.ingg.reports.dailystatusreport;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DailyStatusReportApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
