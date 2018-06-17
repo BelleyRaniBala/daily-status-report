f02b3a45272c6f449556e3b20548ab2f891884e7
Belley Bala <balabelley@gmail.com>
2018-06-17 11:52:18 +0100

    Adding scheduling

--- src/main/java/com/ingg/reports/dailystatusreport/DailyStatusReportApplication.java
+++ src/main/java/com/ingg/reports/dailystatusreport/DailyStatusReportApplication.java
@@ -2,8 +2,10 @@ package com.ingg.reports.dailystatusreport;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class DailyStatusReportApplication {
 
 	public static void main(String[] args) {
